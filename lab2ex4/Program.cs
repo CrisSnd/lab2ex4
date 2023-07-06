09cf6f4e8c2aba6db790474e3b32a7e8a019c3f0
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-06 20:15:06 +0300

    Add project files.

---
+++ lab2ex4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va determina daca un numar este sau nu palindrom.*/
+
+            int numar = 0;
+            int r = 0;
+            int suma = 0;
+            int z = 0;
+            Console.WriteLine("Introduceti valoarea:");
+            while (int.TryParse(Console.ReadLine(), out numar))
+            {
+                z = numar;
+
+                while (numar > 0)
+                 {
+                    r = numar % 10;
+                    suma = (suma * 10) + r;
+                    numar = numar / 10;
+                 }
+
+
+
+                if (z == suma)
+                {
+                    Console.WriteLine($"Numarul {z} este palindrome.");
+                }
+
+                else
+
+                {
+                    Console.WriteLine($"Numarul {z} nu este palindrome.");
+  
+                }
+
+            }
+            
+         }
+    }
+}
